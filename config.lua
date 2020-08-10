a933cbe87b74240bf20da7a5b97ab2dc0fb2a3d2
jrence <60640873+jrence@users.noreply.github.com>
2020-08-11 06:05:16 +0800

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,73 @@
+Config = {}
+local Keys = {
+	["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
+	["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
+	["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
+	["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
+	["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
+	["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
+	["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
+	["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
+	["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
+}
+Config.Locale = 'en'
+
+--Config.serverLogo = 'https://imgur.com/VVzG2Yx.png'
+
+Config.font = {
+	name 	= 'Roboto Condensed',
+	url 	= 'https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap'
+}
+
+
+Config.date = {
+	format	 	= 'default',
+	AmPm		= true
+}
+
+Config.voice = {
+
+	levels = {
+		default = 5.0,
+		shout = 12.0,
+		whisper = 1.0,
+		current = 0
+	},
+	
+	keys = {
+		distance 	= 'H',
+	}
+}
+
+
+Config.vehicle = {
+	speedUnit = 'KMH',
+	maxSpeed = 320,
+
+	keys = {
+		seatbelt 	= 'B',
+		cruiser		= 'CAPS',
+		signalLeft	= 'LEFT',
+		signalRight	= 'RIGHT',
+		signalBoth	= 'UP',
+	}
+}
+
+Config.ui = {
+
+	showSocietyMoney	= true,
+	
+	showLocation 		= true,
+	showVoice	 		= false,
+
+	showHealth			= true,
+	showArmor	 		= true,
+	showStamina	 		= true,
+	showHunger 			= true,
+	showThirst	 		= true,
+	showDrunk			= true,
+
+	showMinimap			= false,
+
+	showWeapons			= false,	
+}
